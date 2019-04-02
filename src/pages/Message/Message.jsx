cd2c4fd7667554f3524adc3ec1018befa656d2e3
leeexing <674023427@qq.com>
2019-04-02 14:28:43 +0800

    :bug: 添加react hook的使用
    测试学习

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,38 +1,88 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Button, Tag } from 'antd'
 import { DRIcon } from '@/icon'
 
 
 function Message () {
   const [count, setCount] = useState(0)
+  const preCount = usePrevious(count)
+  const forceUdate = useForceUpdate()
+  const isMounted = useIsMounted()
 
-  // const handleClick = () => {
-  //   let n = count + 1
-  //   setCount(n)
-  // }
-
-  useEffect(() => {
+  useMount(() => {
     console.log('start...')
-  }, [])
+    console.log(isMounted)
+  })
 
-  useEffect(() => {
-    console.log(count)
+  useUnmount(() => {
+    console.log('unmount~~~')
+    console.log(isMounted)
   })
 
   useEffect(() => {
+    console.log(count)
+    console.log(preCount, 'pre')
+  })
+
+  useUpdate(() => {
     console.log(count + '-Tee')
+    console.log(isMounted)
   })
 
   return (
     <div className="m-message">
-      <h3>个人消息</h3>
-      <div>
-        <DRIcon style={{fontSize: '28px'}}></DRIcon>
-        <Tag>{count}</Tag>
+      <h3>个人消息 <DRIcon style={{fontSize: '28px'}}></DRIcon></h3>
+      <div style={{padding: '10px'}}>
+        <Tag>{preCount} - {count}</Tag>
       </div>
       <Button type="primary" onClick={() => setCount(count + 2)}>点击</Button>
+      <Button type="danger" onClick={() => forceUdate(Date.now())}>强制刷新</Button>
     </div>
   )
+}
+
+function useMount (fn) {
+  useEffect(() => {
+    fn()
+  }, [])
+}
+
+function useUnmount (fn) {
+  useEffect(() => fn, [])
+}
+
+function useUpdate (fn) {
+  const mounting = useRef(true)
+  useEffect(() => {
+    if (mounting.current) {
+      mounting.current = false
+    } else {
+      fn()
+    }
+  })
+}
+
+function useForceUpdate () {
+  return useState(0)[1]
+}
+
+function useIsMounted () {
+  const [isMount, setIsMount] = useState(false)
+  useEffect(() => {
+    if (!isMount) {
+      setIsMount(true)
+    }
+    return () => setIsMount(false)
+  }, [])
+  return isMount
+}
+
+function usePrevious (value) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
 }
 
 export default Message
