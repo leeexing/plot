b2b17e0671b677f67bbb2ec961f4b416abc68843
leeexing <674023427@qq.com>
2019-04-03 15:48:30 +0800

    :sparkles: (testHooks) 使用useReducer构建微型的redux

--- src/pages/TestRedux/TestHooks.jsx
+++ src/pages/TestRedux/TestHooks.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useEffect, useReducer} from 'react'
 
+function reducer (state, action) {
+  switch (action.type) {
+    case 'TICK':
+      let newState = {
+        ...state,
+        count: state.count + action.data
+      }
+      return newState
 
-function TestHooks (props) {
+      default:
+        return state
+  }
+}
+
+function TestHooks () {
+  const [state, dispatch] = useReducer(reducer, {count: 0, step: 0})
+  const {count, step} = state
+  console.log(state)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      dispatch({type: 'TICK', data: 1})
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [dispatch])
+
   return (
     <div>
       <h1>Test</h1>
+      <p>{count} - {step}</p>
     </div>
   )
 }
--- src/storeRedux/reducers/index.js
+++ src/storeRedux/reducers/index.js
@@ -1,3 +1,7 @@
+/**
+ * NOTE:
+ * 使用了combineReducers之后，state的键值可能会改变，要看 reducers 是否还和state.js里面的键值保持一致
+*/
 import { combineReducers } from 'redux'
 import * as pageinfoReducers from './pageinfo'
 import * as todolistReducers from './todolist'
