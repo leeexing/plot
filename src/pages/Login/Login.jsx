e0dc5844f25a10e9919154a1298f24bf00e586cb
leeexing <674023427@qq.com>
2019-06-19 18:42:46 +0800

    :lipstick: 新的登录首页添加屏幕自适应
    :lipstick: 进一步优化登录首页的屏幕适配

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -34,43 +34,45 @@ class Login extends Component {
     return (
       <div className="app-login" style={{backgroundImage: `url(${logo})`}}>
         <div className="app-login-wrap">
-          <h1 className="app-login-header">在线标注平台</h1>
-          <Form onSubmit={this.handleSubmit} className="login-form">
-            <FormItem
-              label="账号"
-            >
-              {getFieldDecorator('username', {
-                rules: [{ required: true, message: 'Please input your username!' }],
-              })(
-                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="邮箱/手机号/用户名" />
-              )}
-            </FormItem>
-            <FormItem
-              label="密码"
-            >
-              {getFieldDecorator('password', {
-                rules: [{ required: true, message: 'Please input your Password!' }],
-              })(
-                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-              )}
-            </FormItem>
-            <FormItem>
-              {getFieldDecorator('remember', {
-                valuePropName: 'checked',
-                initialValue: true,
-              })(
-                <Checkbox>下次自动登录</Checkbox>
-              )}
-              {/* <a className="login-form-forgot" href="/">忘记密码了</a> */}
-              <Button type="primary" htmlType="submit" className="login-form-button">
-                登录
-              </Button>
-            </FormItem>
-          </Form>
-          {/* <div className="third-part">
-            第三方登录:
-            <Icon type="slack" theme="outlined" />
-          </div> */}
+          <div className="app-login-form">
+            <h1 className="app-login-header app-login-title">在线标注平台</h1>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+              <FormItem
+                label="账号"
+              >
+                {getFieldDecorator('username', {
+                  rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                  <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="邮箱/手机号/用户名" />
+                )}
+              </FormItem>
+              <FormItem
+                label="密码"
+              >
+                {getFieldDecorator('password', {
+                  rules: [{ required: true, message: 'Please input your Password!' }],
+                })(
+                  <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+              </FormItem>
+              <FormItem>
+                {getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                })(
+                  <Checkbox>下次自动登录</Checkbox>
+                )}
+                {/* <a className="login-form-forgot" href="/">忘记密码了</a> */}
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  登录
+                </Button>
+              </FormItem>
+            </Form>
+            {/* <div className="third-part">
+              第三方登录:
+              <Icon type="slack" theme="outlined" />
+            </div> */}
+          </div>
         </div>
       </div>
     )
